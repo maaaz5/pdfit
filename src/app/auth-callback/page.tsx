f5336edb879dbf4870176558e86b263940945c2c
maaaz5 <mohamade.amine12345@gmail.com>
2023-12-14 00:00:26 +0100

    authcallback query setup

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,10 +1,22 @@
 import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
 
 const Page = () => {
   const router = useRouter();
 
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
+
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        //user is synced to db
+        router.push(origin ? `/${origin}` : "/dashboard");
+      }
+    },
+  });
+
+  return <div>Hello</div>;
 };
 
 export default Page;
